efd75f88beb589665eff1166439bb31be374f8c1
Ada Freya Ahmed <ada@chronovore.dev>
2026-05-18 08:20:41 +0530

    unpack: fix resources overwriting each other

--- Ulysses.DPLUnpack/Program.cs
+++ Ulysses.DPLUnpack/Program.cs
@@ -136,7 +136,7 @@ bool ProcessFHMItem(FHMFile fhm, FHMItemHeader itemHeader, string path, string n
 			var didConvert = true;
 
 			for (var resourceIndex = 0; resourceIndex < resource.ResourceCount; ++resourceIndex) {
-				var resourceName = resource.GetResourceName(resourceIndex, resource.ResourceCount == 1 && (fhm.Count == 1 || resource.IsFullyUtilized) ? string.Empty : $"/{resourceIndex}");
+				var resourceName = resource.GetResourceName(resourceIndex, resource.ResourceCount == 1 && (fhm.Count == 1 || resource.IsFullyUtilized) ? string.Empty : $"/{itemIndex}");
 				if (resourceName == null) {
 					continue;
 				}
