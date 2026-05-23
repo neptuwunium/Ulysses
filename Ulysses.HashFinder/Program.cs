21a046c054fe699c95a6cf480607a7023db2f157
Ada Freya Ahmed <ada@chronovore.dev>
2026-05-23 11:12:30 +0530

    names: more names yippee
    we have all cinematic names now.
    
    17 map names missing
    
    11 generic names missing
    
    11 debug names missing
    
    99 2dimage names missing

--- Ulysses.HashFinder/Program.cs
+++ Ulysses.HashFinder/Program.cs
@@ -7,7 +7,6 @@ using Ulysses.Struct;
 
 var dplHash = new HashSet<uint>();
 var textHash = new HashSet<uint>();
-var dplPrefix = IdRegistry.Lookup.Where(x => x.Value.StartsWith("DPL_")).Select(x => x.Value).ToHashSet();
 
 if (File.Exists("DplHash.txt")) {
 	LoadHashFile("DplHash.txt", dplHash);
@@ -22,75 +21,6 @@ foreach (var id in IdRegistry.Lookup.Keys) {
 	textHash.Remove(id);
 }
 
-if (dplHash.Count > 0) {
-	for (var i = 0; i < 1000; i++) {
-		TestDplHash($"DPL_EMBLEM_{i:D3}");
-		TestDplHash($"DPL_2DIMAGE_EMB_{i:D3}");
-		TestDplHash($"DPL_2DIMAGE_EMBL_{i:D3}");
-		TestDplHash($"DPL_2DIMAGE_AC_{i:D}");
-		TestDplHash($"DPL_2DIMAGE_MIS_{i:D}");
-	}
-
-	for (var i = 0; i < 10000; i++) {
-		TestDplHash($"DPL_2DIMAGE_GIFT_{i:D4}");
-	}
-
-	for (var i = 0; i < 100; i++) {
-		TestDplHash($"DPL_DLC_CATALOG_{i}");
-		TestDplHash($"DPL_2DIMAGE_GIFT_{i}");
-		TestDplHash($"DPL_2DIMAGE_PARTS_{i}");
-		TestDplHash($"DPL_2DIMAGE_GUIDE_{i}");
-		TestDplHash($"DPL_2DIMAGE_TIPS_{i}");
-		TestDplHash($"DPL_UI_LOADING_TIP{i}");
-		for (var l = 'A'; l <= 'Z'; ++l) {
-			TestDplHash($"DPL_MIC{l}{i:D2}_RES");
-			TestDplHash($"DPL_MIC{l}{i:D2}_SCRIPT");
-			TestDplHash($"DPL_MIC{l}{i:D2}_LUASCRIPT");
-		}
-
-		TestDplHash($"DPL_P_USERDATA_T{i:D2}");
-		TestDplHash($"DPL_MAP_MS{i:D2}");
-		TestDplHash($"DPL_MAP_MS{i:D2}_MPT");
-	}
-
-	TestDplHash("DPL_DEVELOPMENT");
-	TestDplHash("DPL_INFORMATION");
-	TestDplHash("DPL_TSS_MISC");
-	TestDplHash("DPL_TSS_INFO");
-	TestDplHash("DPL_TSS_DROP_ITEM");
-	TestDplHash("DPL_TSS_SALES_LIST");
-	TestDplHash("DPL_TSS_ITEM");
-	TestDplHash("DPL_UI_STARTUP");
-	TestDplHash("DPL_UI_TSS_COMMON");
-	TestDplHash("DPL_UI_TSS_MENU");
-	TestDplHash("DPL_MAP");
-	TestDplHash("DPL_MAP_MPT");
-	TestDplHash("DPL_RES");
-	TestDplHash("DPL_SCRIPT");
-	TestDplHash("DPL_LUASCRIPT");
-	TestDplHash("DPL_CINEMA");
-	TestDplHash("DPL_CINEMA_S01");
-	TestDplHash("DPL_COMMON");
-	TestDplHash("DPL_MAP_COMMON");
-	TestDplHash("DPL_POSTPROCESS");
-
-	TestDplHash("DPL_CINEMA_CD_HANGAR_04");
-	TestDplHash("DPL_CINEMA_CD_HANGAR_04_S01");
-
-	for (var i = 0; i < 100; ++i) {
-		for (var j = 0; j < 100; ++j) {
-			for (var l = 'A'; l <= 'Z'; ++l) {
-				for (var l2 = 'A'; l2 <= 'Z'; ++l2) {
-					TestDplHash($"DPL_CINEMA_DD_MIC{l}{i:D2}_{j:D2}{l2}");
-					TestDplHash($"DPL_CINEMA_DD_MIC{l}{i:D2}_{j:D2}{l2}_S01");
-					TestDplHash($"DPL_CINEMA_DD_MIT{l}{i:D2}_{j:D2}{l2}");
-					TestDplHash($"DPL_CINEMA_DD_MIT{l}{i:D2}_{j:D2}{l2}_S01");
-				}
-			}
-		}
-	}
-}
-
 var lines = new HashSet<string>();
 var linesUpper = new HashSet<string>();
 using (var fs = new StreamReader(new FileStream("names.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))) {
@@ -101,6 +31,8 @@ using (var fs = new StreamReader(new FileStream("names.txt", FileMode.Open, File
 		}
 	}
 }
+
+linesUpper.RemoveWhere(x => x.Contains(' ', StringComparison.Ordinal) || x[0] is '_' || x[^1] is '_');
 
 using (var fs = new StreamReader(new FileStream("Strings.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))) {
 	while (fs.ReadLine() is { } line) {
@@ -115,11 +47,11 @@ using (var fs = new StreamReader(new FileStream("Strings.txt", FileMode.Open, Fi
 }
 
 if (textHash.Count > 0) {
-	Parallel.ForEach(lines, TestLvstHashFormat);
+	Parallel.ForEach(lines, TestLvstHashFormatOuter);
 }
 
 if (dplHash.Count > 0) {
-	Parallel.ForEach(linesUpper, TestDplHashFormat);
+	Parallel.ForEach(linesUpper, TestDplHashFormatRecurOuter);
 }
 
 return;
@@ -153,95 +85,68 @@ void TestLvstHash(string text) {
 void TestLvstHashFormat(string text) {
 	TestLvstHash(text);
 	TestLvstHash(text + "Hash");
+	TestLvstHash(text + "HashID");
 	TestLvstHash(text + "FileId");
+	TestLvstHash(text + "FileID");
 	TestLvstHash(text + "File");
 	TestLvstHash(text + "DPL");
+	TestLvstHash(text + "DPLID");
 	TestLvstHash(text + "Id");
 	TestLvstHash(text + "Dpl");
 	TestLvstHash(text + "ID");
 	TestLvstHash(text + "MsgId");
+	TestLvstHash(text + "MsgID");
 	TestLvstHash(text + "IdStr");
 	TestLvstHash(text + "IDStr");
 }
 
+void TestLvstHashFormatRecur(string text, int nest) {
+	foreach (var piece in lines) {
+		var pt = text + piece;
+		TestLvstHashFormat(pt);
+
+		if (nest > 0) {
+			TestLvstHashFormatRecur(pt + "_", nest - 1);
+		}
+	}
+}
+
+void TestLvstHashFormatOuter(string text) {
+	TestLvstHashFormat(text);
+	TestLvstHashFormatRecur(text + "_", 0);
+}
+
+
 void TestDplHashFormat(string text) {
-	text = text.ToUpper();
 	TestDplHash("DPL_" + text);
-	for (var i = 0; i < 100; i++) {
-		TestDplHash($"DPL_P_{text}_T{i:D2}");
-		TestDplHash($"DPL_P_{text}_E{i:D2}");
-		TestDplHash($"DPL_D_{text}_T{i:D2}");
-		TestDplHash($"DPL_D_{text}_E{i:D2}");
-		TestDplHash($"DPL_CINEMA_{text}_VO_{i:D}");
-		TestDplHash($"DPL_CINEMA_{text}_VO_{i:D3}");
+	TestDplHash("DPL_UI_" + text);
+	TestDplHash("DPL_TSS_" + text);
+	TestDplHash("DPL_UI_TSS_" + text);
+	TestDplHash("DPL_MAP_" + text);
+	TestDplHash("DPL_2DIMAGE_" + text);
+
+	for (var i = 0; i < 99; ++i) {
+		TestDplHash("DPL_MAP_" + text + $"{i:D2}");
+		TestDplHash("DPL_" + text + $"{i:D2}");
 	}
 
-	foreach (var prefix in dplPrefix) {
-		TestDplHash($"{prefix}_{text}");
-		TestDplHash($"{prefix}_VO_{text}");
-		TestDplHash($"{prefix}_SE_{text}");
-		TestDplHash($"{prefix}_VO_S01_{text}");
-		TestDplHash($"{prefix}_SE_S01_{text}");
+	for (var i = 0; i < 3000; ++i) {
+		TestDplHash("DPL_2DIMAGE_" + text + $"_{i}");
 	}
+}
 
-	TestDplHash($"DPL_UI_{text}");
-	TestDplHash($"DPL_UI_{text}_1");
-	TestDplHash($"DPL_2DIMAGE_{text}");
-	TestDplHash($"DPL_2DIMAGE_{text}_1");
-	TestDplHash($"DPL_2DIMAGE_{text}_01");
-	TestDplHash($"DPL_2DIMAGE_{text}_001");
-	TestDplHash($"DPL_2DIMAGE_{text}_0001");
-	TestDplHash($"DPL_UI_{text}");
-	TestDplHash($"DPL_UI_{text}_1");
-	TestDplHash($"DPL_UI_{text}_01");
-	TestDplHash($"DPL_UI_{text}_001");
-	TestDplHash($"DPL_UI_{text}_0001");
+void TestDplHashFormatRecur(string text, int nest) {
+	foreach (var piece in linesUpper) {
+		var pt = text + piece;
+		TestDplHashFormat(pt);
 
-	TestDplHash($"DPL_P_{text}_EMBLEM");
-	TestDplHash($"DPL_D_{text}");
-	TestDplHash($"DPL_D_{text}_EMBLEM");
-	TestDplHash($"DPL_P_{text}_K");
-	TestDplHash($"DPL_P_{text}");
-	TestDplHash($"DPL_MAP_{text}");
-	TestDplHash($"DPL_MAP_{text}_MPT");
-	TestDplHash($"DPL_BGM_{text}");
-	TestDplHash($"DPL_{text}_BGM");
-	TestDplHash($"DPL_{text}_RES");
-	TestDplHash($"DPL_{text}_SCRIPT");
-	TestDplHash($"DPL_{text}_LUASCRIPT");
-	TestDplHash($"DPL_CINEMA_{text}_S01");
-	TestDplHash($"DPL_CINEMA_{text}");
-	TestDplHash($"DPL_CINEMA_DD_{text}_S01");
-	TestDplHash($"DPL_CINEMA_DD_{text}");
-	TestDplHash($"DPL_CINEMA_CD_{text}_S01");
-	TestDplHash($"DPL_CINEMA_CD_{text}");
-	TestDplHash($"DPL_CINEMA_DD_{text}_VO_S01");
-	TestDplHash($"DPL_CINEMA_DD_{text}_VO");
-	TestDplHash($"DPL_CINEMA_CD_{text}_SE_S01");
-	TestDplHash($"DPL_CINEMA_CD_{text}_SE");
-	TestDplHash($"DPL_CINEMA_DD_{text}_01a_S01");
-	TestDplHash($"DPL_CINEMA_DD_{text}_01a");
-	TestDplHash($"DPL_CINEMA_CD_{text}_01a_S01");
-	TestDplHash($"DPL_CINEMA_CD_{text}_01a");
-	TestDplHash($"DPL_CINEMA_DD_{text}_01a_VO_S01");
-	TestDplHash($"DPL_CINEMA_DD_{text}_01a_VO");
-	TestDplHash($"DPL_CINEMA_CD_{text}_01a_SE_S01");
-	TestDplHash($"DPL_CINEMA_CD_{text}_01a_SE");
-	TestDplHash($"DPL_{text}_S01");
-	TestDplHash($"DPL_DD_{text}_S01");
-	TestDplHash($"DPL_DD_{text}");
-	TestDplHash($"DPL_CD_{text}_S01");
-	TestDplHash($"DPL_CD_{text}");
-	TestDplHash($"DPL_DD_{text}_VO_S01");
-	TestDplHash($"DPL_DD_{text}_VO");
-	TestDplHash($"DPL_CD_{text}_SE_S01");
-	TestDplHash($"DPL_CD_{text}_SE");
-	TestDplHash($"DPL_DD_{text}_01a_S01");
-	TestDplHash($"DPL_DD_{text}_01a");
-	TestDplHash($"DPL_CD_{text}_01a_S01");
-	TestDplHash($"DPL_CD_{text}_01a");
-	TestDplHash($"DPL_DD_{text}_01a_VO_S01");
-	TestDplHash($"DPL_DD_{text}_01a_VO");
-	TestDplHash($"DPL_CD_{text}_01a_SE_S01");
-	TestDplHash($"DPL_CD_{text}_01a_SE");
+		if (nest > 0) {
+			TestDplHashFormatRecur(pt + "_", nest - 1);
+		}
+	}
+}
+
+void TestDplHashFormatRecurOuter(string text) {
+	TestDplHashFormat(text);
+	TestDplHashFormatRecur(text + "_", 0);
 }
